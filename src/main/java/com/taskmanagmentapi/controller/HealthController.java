91d896f1795467034ccce29cc5aafc82643b189f
MertKocoglu <mertkocoglu3@gmail.com>
2026-06-28 21:00:25 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/taskmanagmentapi/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.taskmanagmentapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Task Management API is running";
+    }
+}
